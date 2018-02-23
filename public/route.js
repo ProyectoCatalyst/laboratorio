598fe83431b46c18f03b5febe1cdd17cae860e74
isaacSolano <35480414+isaacSolano@users.noreply.github.com>
2018-02-23 16:25:32 -0600

    agregar vista de administrador seccion retoques
    Se agrega la vista donde el administrador podrá agregar, modificar, o eliminar retoques

--- public/route.js
+++ public/route.js
@@ -15,6 +15,25 @@
         url: '/',
         templateUrl: './components/landingPage/landingPage.view.html'
       });
+
+      // generar vista de retoques de administrador, donde se dara mantenimiento a la parte de agregar, remover o modificar servicios. Calidad 1 | Isaac.
+
+      $stateProvider
+        .state('retoques', {
+          url: '/retoques',
+          templateUrl: './components/retoques/retoquesAdm.view.html',
+          data:{
+            pageTitle: 'Laboratorio | Retoques'
+          },
+          resolve:{
+            load: ['$ocLazyLoad', () => {
+              return $ocLazyLoad.load('./components/retoques/retoquesAdm.controller.js')
+            }]
+          },
+          controller: 'controladorAdmRetoques',
+          controllerAs: 'vm'
+        });
+
     //Por el amor de Dios comenten esto si surge un problema raro y corran la aplicación :)
     $urlRouterProvider.otherwise('/');
   };
