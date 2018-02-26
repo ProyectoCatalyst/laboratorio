826215f0caa23ed8afbec7f690913ef9df17011c
varo92 <35508250+varo92@users.noreply.github.com>
2018-02-26 12:34:29 -0600

    Fiesta
    Agregue servicio fiesta

---
+++ public/components/services/party.service.js
@@ -0,0 +1,47 @@
+(() => {
+  'use strict';
+  angular
+    .module('laboratorio')
+    .service('servicioUsuarios', servicioUsuarios);
+
+  servicioUsuarios.$inject = ['$q', '$log', '$http']; 
+
+    function _agregarUsuario(pusuario) {
+
+      let todosLosUsuarios = _retornarUsuario();
+      let registroExitoso = true;
+
+      todosLosUsuarios.push(pusuario);
+
+      asyncLocalStorage.setItem('listaUsuariosLS', todosLosUsuarios).then((result) => {
+        registroExitoso = result
+      });
+
+      return registroExitoso;
+    }
+
+    function _retornarDifunto(pusuario) {
+        
+      
+    }
+
+    function _retornarUsuario() {
+      let todosLosUsuarios = [],
+
+        listaUsuarios = JSON.parse(localStorage.getItem('listaUsuariosLS'));
+
+      if (listaUsuarios == null) {
+
+        return todosLosUsuarios;
+
+      } else {
+
+        listaUsuarios.forEach(obj => {
+
+          let objUsuarioTemp = new Usuario(obj.nombre, obj.primerApellido, obj.segundoApellido, obj.cedula, obj.fecha, obj.genero, obj.foto, obj.ubicacion, obj.provincia, obj.canton, obj.distrito, obj.usuario, obj.correo, obj.contrasenna);
+
+          todosLosUsuarios.push(objUsuarioTemp);
+        });
+      }
+      return todosLosUsuarios;
+    }
